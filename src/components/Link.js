8bcae6432d9820254ce4a93d3b7e5e3bfbcbd474
Nutsa Kharazishvili <nutsa1999@gmail.com>
2022-08-07 17:12:35 +0400

    initial commit

---
+++ src/components/Link.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Link = ({ className, href, children }) => {
+    const onClick = (e) => {
+        // ctrl click - > new tab
+        if (e.metaKey || e.ctrlKey) {
+            return;
+        }
+        e.preventDefault();
+        window.history.pushState({}, '', href);
+
+        const navEvent = new PopStateEvent('popstate');
+        window.dispatchEvent(navEvent);
+    };
+
+    return (
+        <a onClick={onClick} className={className} href={href} >
+            {children}
+        </a>
+    )
+}
+
+export default Link;
